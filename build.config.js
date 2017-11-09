04f24a56c048a13bde91145480cddc214154ed77
Igibek Koishybayev <koishybayev.igibek@gmail.com>
2017-11-09 00:07:24 -0500

    updated for fast built

--- build.config.js
+++
@@ -1,3 +0,0 @@
-//change to false to fail the build
-const isBuildSuccess = true;
-module.exports = isBuildSuccess;
--- envTest.js
+++
@@ -1,3 +0,0 @@
-
-console.log("SLACKTOKEN:", process.env.ALTCODETOKEN);
-console.log("FORECASTTOKEN:", process.env.FORECASTTOKEN);
